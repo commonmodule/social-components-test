ac7984f341d402733c9c1f3674a867e93b6c84e3
yjgaia <yjdev88@gmail.com>
2024-10-11 15:01:55 +0900

    Refactor styles and file structure

--- webpack.config.ts
+++ webpack.config.ts
@@ -37,7 +37,7 @@ const config: webpack.Configuration = {
   },
   output: {
     filename: "[name].js",
-    path: path.resolve("public"),
+    path: path.resolve("docs"),
   },
   plugins: [
     new MiniCssExtractPlugin({
