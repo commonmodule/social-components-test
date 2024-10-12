4d926d59d4042c9cebce27ed4d06b11dfa50604c
yjgaia <yjdev88@gmail.com>
2024-10-12 14:42:51 +0900

    Refactor main.ts and webpack.config.ts to add functionality for displaying author information in the chat view

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,15 @@
 import { Router } from "@common-module/app";
 import ChatView from "./ChatView.js";
+import { SocialCompConfig } from "@common-module/social-components";
 
-(() => {
-  Router.add("/chat", ChatView);
-})();
+SocialCompConfig.showAuthorInfo = (author) => {
+  const message = `Author Information:
+-------------------
+Name: ${author.name}
+ID: ${author.id}
+Avatar: ${author.avatarUrl}`;
+
+  alert(message);
+};
+
+Router.add("/chat", ChatView);
